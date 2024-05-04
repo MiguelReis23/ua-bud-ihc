82bf951f40692e802c2bfae8e927dd8f195c8268
miguelovila <miguelovila@ua.pt>
2024-05-04 05:33:55 +0100

    [WIP] Navbar

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,8 +1,28 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
+import { Icons } from '@/components/icons';
+
 export function SiteHeader({ dictionary } : { dictionary: any }) {
     return (
         <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60" >
-            <div className="container flex h-14 max-w-screen-2xl items-center" >
-                {dictionary.test}
+            <div className="container flex max-w-screen-2xl items-center py-2">
+                <div className="flex flex-1 items-center justify-between space-x-2 md:jusitfy-end">
+                    <Icons.logo className="h-full w-auto mt-1" />
+                    <nav className="flex items-center space-x-4">
+                        <Button variant="secondary">
+                            <Icons.history className="mr-2" /> {dictionary.myTickets.toUpperCase()}
+                        </Button>
+                        <Button variant="default">
+                            <Icons.add className="mr-1" /> {dictionary.newTicket.toUpperCase()}
+                        </Button>
+                        <Separator orientation="vertical" className="h-8" />
+                        <Avatar>
+                            <AvatarImage src="https://avatars.githubusercontent.com/u/38387065?v=4" />
+                            <AvatarFallback>MV</AvatarFallback>
+                        </Avatar>
+                    </nav>
+                </div>
             </div>
         </header>
     );
