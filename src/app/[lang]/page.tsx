3164a67313ee71504f1bec45a048edb227ed2f76
Miguel Vila <miguelovila@ua.pt>
2024-05-13 18:59:33 +0100

    [NEW] Site footer

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,45 +1,47 @@
-import { LandingCarousel } from "@/components/landing-carousel"
-import { SiteHeader } from "@/components/site-header"
-import { getDictionary } from "@/lib/get-dictionary"
-import { LandingCard } from "@/components/landing-card"
-import Link from 'next/link'
+import { LandingCarousel } from "@/components/landing-carousel";
+import { SiteHeader } from "@/components/site-header";
+import { getDictionary } from "@/lib/get-dictionary";
+import { LandingCard } from "@/components/landing-card";
+import Link from "next/link";
+import { SiteFooter } from "@/components/site-footer";
 
-export default async function Home({ params: { lang } }: { params: { lang: string } }) {
+export default async function Home({
+  params: { lang },
+}: {
+  params: { lang: string };
+}) {
+  const dictionary = await getDictionary(lang);
 
-    const dictionary = await getDictionary(lang)
-
-    return (
-        <>
-            <SiteHeader dictionary={dictionary} />
-            <main className="flex-1">
-                <LandingCarousel dictionary={dictionary} />
-                <div className="max-w-7-xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-8">
-                        <section>
-                            <h3 className="text-2xl font-semibold mb-6">Artigos de Ajuda Populares</h3>
-                            <LandingCard
-                                title="Teste"
-                                description="Teste de descrição" />
-                            <LandingCard
-                                title="Teste2"
-                                description="Teste de descrição" />
-                            <Link className="block mt-4 text-blue-700 hover:text-blue-500" href="#">
-                                {`Ler mais artigos >`}
-                            </Link>
-                                
-                        </section>
-                        <section>
-                            <h3 className="text-2xl font-semibold mb-6">Últimas Novidades</h3>
-                            <LandingCard
-                                title="Teste3"
-                                description="Teste de descrição" />
-                            <LandingCard
-                                title="Teste4"
-                                description="Teste de descrição" />
-                        </section>
-                    </div>
-                </div>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <SiteHeader dictionary={dictionary} />
+      <main className="">
+        <LandingCarousel dictionary={dictionary} />
+        <div className="container flex-1 max-w-screen-2xl max-w-7-xl mx-auto py-8">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 py-8">
+            <section>
+              <h3 className="text-2xl font-semibold mb-6">
+                Artigos de Ajuda Populares
+              </h3>
+              <LandingCard title="Teste" description="Teste de descrição" />
+              <LandingCard title="Teste2" description="Teste de descrição" />
+              <LandingCard title="Teste2" description="Teste de descrição" />
+              <Link
+                className="block mt-4 text-blue-700 hover:text-blue-500"
+                href="#"
+              >
+                {`Ler mais artigos >`}
+              </Link>
+            </section>
+            <section>
+              <h3 className="text-2xl font-semibold mb-6">Últimas Novidades</h3>
+              <LandingCard title="Teste3" description="Teste de descrição" />
+              <LandingCard title="Teste4" description="Teste de descrição" />
+            </section>
+          </div>
+        </div>
+      </main>
+      <SiteFooter dictionary={dictionary} />
+    </>
+  );
 }
---
+++ src/components/site-footer.tsx
@@ -0,0 +1,64 @@
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/icons";
+import Link from "next/link";
+
+export function SiteFooter({ dictionary }: { dictionary: any }) {
+  return (
+    <footer className="w-full border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-4">
+      <section className="container max-w-screen-2xl items-center py-2 flex flex-col md:flex-row items-center justify-between mb-4">
+        <div className="flex flex-col space-y-1 md:space-y-0 md:flex-row md:space-x-4">
+          <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+            <a
+              href="privacy-policy"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              {dictionary.privacyPolicy}
+            </a>
+          </p>
+
+          <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+            <a
+              href="terms-of-service"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              {dictionary.termsOfService}
+            </a>
+          </p>
+
+          <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+            <a
+              href="sitemap"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              {dictionary.siteMap}
+            </a>
+          </p>
+        </div>
+
+        <div>
+          <p className="mt-2 md:mt-0">
+            © {new Date().getFullYear()} {dictionary.copyRight}
+          </p>
+        </div>
+      </section>
+      <section className="border-t border-border/40 container max-w-screen-2xl items-center py-2 flex flex-col">
+        <Image
+          src={"/footer/sponsors.png"}
+          alt={"Project sponsors"}
+          objectFit="cover"
+          quality={70}
+          width={400}
+          height={100}
+        />
+      </section>
+    </footer>
+  );
+}
