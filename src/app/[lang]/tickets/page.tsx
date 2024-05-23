bb18dc3b5c431521c0b61f8e8b83f5b7d06a985e
Miguel Vila <miguelovila@ua.pt>
2024-05-24 00:06:18 +0100

    [WIP] Tickets organization and ordering
    Co-authored-by: Miguel Reis <mreis@ua.pt>

--- src/app/[lang]/tickets/page.tsx
+++ src/app/[lang]/tickets/page.tsx
@@ -1,10 +1,31 @@
 import { SiteHeader } from "@/components/site-header";
-import { getDictionary } from "@/lib/get-dictionary"
+import { getDictionary } from "@/lib/get-dictionary";
 import { Icons } from "@/components/icons";
 import TicketsData from "@/data/tickets.json";
 import Link from "next/link";
 
-export default async function Tickets({ params: { lang } }: { params: { lang: string } }) {
+import { promises as fs } from "fs";
+import path from "path";
+import { z } from "zod";
+import { taskSchema } from "@/data/tasksSchema";
+import { columns } from "@/components/ui/columns";
+import { DataTable } from "@/components/ui/data-table";
+
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "src/data/tasks.json")
+  );
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
+
+export default async function Tickets({
+  params: { lang },
+}: {
+  params: { lang: string };
+}) {
   const ticketInfo = TicketsData.map((ticket, index) => ({
     id: ticket.id,
     subject: ticket.subject,
@@ -16,7 +37,9 @@ export default async function Tickets({ params: { lang } }: { params: { lang: st
     lastMessage: ticket.lastMessage,
   }));
 
-  const dictionary = await getDictionary(lang)
+  const dictionary = await getDictionary(lang);
+
+  const tasks = await getTasks();
 
   return (
     <>
@@ -24,64 +47,10 @@ export default async function Tickets({ params: { lang } }: { params: { lang: st
       <main className="flex flex-col items-center h-screen py-12 container">
         <div className="w-full px-4 md:px-6">
           <div className="mb-8 text-center">
-            <h1 className="text-4xl font-bold">
-            {dictionary.myTickets}
-            </h1>
-          </div>
-          <div className="overflow-hidden rounded-lg shadow-lg">
-            <div className="w-full">
-              <div className="grid grid-cols-7 gap-4 bg-neutral-200 dark:bg-neutral-800">
-                <div className="px-4 py-3 text-left text-sm font-medium">
-                {dictionary.ticketSubject}
-                </div>
-                <div className="px-4 py-3 text-left text-sm font-medium ">
-                {dictionary.ticketPriority}
-                  <Icons.arrowupdown className="w-6 h-4 inline-block" />
-                </div>
-                <div className="px-4 py-3 text-left text-sm font-medium">
-                {dictionary.ticketDate}
-                  <Icons.arrowupdown className="w-6 h-4 inline-block" />
-                </div>
-                <div className="px-4 py-3 text-left text-sm font-medium">
-                {dictionary.ticketRequester}
-                </div>
-                <div className="px-4 py-3 text-left text-sm font-medium">
-                {dictionary.ticketResponsible}
-                </div>
-                <div className="px-4 py-3 text-left text-sm font-medium">
-                {dictionary.ticketStatus}
-                </div>
-                <div className="px-4 py-3 text-left text-sm font-medium">
-                {dictionary.ticketLastMessage}
-                </div>
-              </div>
-              <div className="divide-y ">
-                {ticketInfo
-                .filter(ticket => ticket.requester === "Kushwaha Chopra")
-                .map((ticket, index) => (
-                  <Link href={`/tickets/${ticket.id}`}>
-                    <div
-                      key={ticket.id}
-                      className={`grid grid-cols-7 gap-4 ${
-                        index % 2 === 0
-                          ? "bg-neutral-100 dark:bg-neutral-700 hover:bg-neutral-100 dark:hover:bg-neutral-600"
-                          : "bg-white dark:bg-neutral-800 hover:bg-neutral-100 dark:hover:bg-neutral-600"
-                      }`}
-                    >
-                      <div className="px-4 py-4">{ticket.subject}</div>
-                      <div className="px-4 py-3">{ticket.priority}</div>
-                      <div className="px-4 py-3">{ticket.date}</div>
-                      <div className="px-4 py-3">{ticket.requester}</div>
-                      <div className="px-4 py-3">{ticket.responsible}</div>
-                      <div className="px-4 py-3">{ticket.status}</div>
-                      <div className="px-4 py-3">{ticket.lastMessage}</div>
-                    </div>
-                  </Link>
-                ))}
-              </div>
-            </div>
+            <h1 className="text-4xl font-bold">{dictionary.myTickets}</h1>
           </div>
         </div>
+        <DataTable data={tasks} columns={columns} />
       </main>
     </>
   );
---
+++ src/data/tickets.tsx
@@ -0,0 +1,12 @@
+import {
+  ArrowDownIcon,
+  ArrowRightIcon,
+  ArrowUpIcon,
+  CheckCircledIcon,
+  CircleIcon,
+  CrossCircledIcon,
+  QuestionMarkCircledIcon,
+  StopwatchIcon,
+} from "@radix-ui/react-icons";
+
+export const labels = [];
